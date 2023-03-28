5c766c369057c50043b1fb4a5804d2be4e610479
DayTeaJun <gkfnck@gmail.com>
2023-03-28 22:14:03 +0900

    23-03-28 두 수의 합.js

---
+++ 23-03-28 두 수의 합.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+
+function solution(num1, num2) {
+    return num1 + num2;
+}
+
+// !문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=javascript
