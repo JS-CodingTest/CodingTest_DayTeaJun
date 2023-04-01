36ee7ae2e236c9e6269aba663e54729e7d71a937
DayTeaJun <gkfnck@gmail.com>
2023-04-01 23:04:42 +0900

    23-04-01 두수의 차

---
+++ Lv0 두 수의 차.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50000 ≤ num1 ≤ 50000
+//-50000 ≤ num2 ≤ 50000
+
+function solution(num1, num2) {
+    return num1 - num2;
+}
+
+// !문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
