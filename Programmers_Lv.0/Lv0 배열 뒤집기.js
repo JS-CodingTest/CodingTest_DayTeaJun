b1cdb7c0b4925f9399343c2372f4e0ac396eb14d
DayTeaJun <gkfnck@gmail.com>
2023-04-16 00:35:17 +0900

    23-04-16 배열 뒤집기

---
+++ Programmers_Lv.0/Lv0 배열 뒤집기.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ num_list의 길이 ≤ 1,000
+// 0 ≤ num_list의 원소 ≤ 1,000
+
+function solution(num_list) {
+    return num_list.reverse()
+}
+// reverse() 배열 순서를 전부 반대로 교체해 준다.
+
+// !문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120821
