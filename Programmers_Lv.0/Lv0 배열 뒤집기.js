494fa3e9763f56e144299acb079b557ff356d903
DayTeaJun <gkfnck@gmail.com>
2023-04-16 00:44:47 +0900

    23-04-16 배열 뒤집기

--- Programmers_Lv.0/Lv0 배열 뒤집기.js
+++ Programmers_Lv.0/Lv0 배열 뒤집기.js
@@ -5,9 +5,20 @@
 // 1 ≤ num_list의 길이 ≤ 1,000
 // 0 ≤ num_list의 원소 ≤ 1,000
 
+// 방법 1
 function solution(num_list) {
     return num_list.reverse()
 }
 // reverse() 배열 순서를 전부 반대로 교체해 준다.
+
+// 방법 2
+function solution(num_list) {
+    var answer = [];
+    var j = num_list.length
+    for (var i = 1; i <= j; i++) {
+        answer.push(num_list[j - i])
+    }
+    return answer;
+}
 
 // !문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120821
