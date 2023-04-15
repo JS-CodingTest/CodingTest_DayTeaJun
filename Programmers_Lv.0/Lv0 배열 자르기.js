d1c5b23925b3ba8c9ce46deace01f44d0a5c8c96
DayTeaJun <gkfnck@gmail.com>
2023-04-16 01:37:04 +0900

    23-04-16 배열 자르기

---
+++ Programmers_Lv.0/Lv0 배열 자르기.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+
+// 제한사항
+// 2 ≤ numbers의 길이 ≤ 30
+// 0 ≤ numbers의 원소 ≤ 1,000
+// 0 ≤num1 < num2 < numbers의 길이
+
+function solution(numbers, num1, num2) {
+    return numbers.slice(num1, num2 + 1)
+}
+
+// !문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120833
