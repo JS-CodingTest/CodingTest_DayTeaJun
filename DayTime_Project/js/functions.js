73194065a6d630f91024a7429f532a7795f79a11
DayTeaJun <gkfnck@gmail.com>
2023-04-18 10:46:24 +0900

    title, content -> textContent modify

--- DayTime_Project/js/functions.js
+++ DayTime_Project/js/functions.js
@@ -4,13 +4,13 @@
 // 각 제목과 내용에 글자색을 넣고, 게시물 올리기 버튼을 누르면
 // 밑에 카드 생성
 
-let inpTitle = document.querySelector('.inp-title');
-let contentText = document.querySelector('.content-textarea');
-let inpBtn = document.querySelector('.inp-btn');// 버튼
-
-let toDay = new Date();
+const inpTitle = document.querySelector('.inp-title'); // 제목
+const contentText = document.querySelector('.content-textarea'); // 내용
+const inpBtn = document.querySelector('.inp-btn'); // 버튼
 
 const sectionContainer = document.querySelector('.section-container');
+
+const today = new Date();
 
 inpBtn.addEventListener('click', function () {
     if (!inpTitle.value)// 제목이 입력되지 않으면 alert 발생
@@ -18,37 +18,37 @@ inpBtn.addEventListener('click', function () {
     else if (!contentText.value)// 내용이 입력되지 않으면 alert 발생
         alert('내용을 입력해 주세요!');
     else {
-        let cardItem = document.createElement('article');
+        const cardItem = document.createElement('article');
         cardItem.setAttribute('class', 'card-item');
         sectionContainer.appendChild(cardItem);
 
-        let cardBorder = document.createElement('a');
+        const cardBorder = document.createElement('a');
         cardBorder.setAttribute('href', '#');
         cardItem.appendChild(cardBorder);
 
-        let cardImg = document.createElement('img');
+        const cardImg = document.createElement('img');
         cardImg.setAttribute('class', 'img');
         cardImg.setAttribute('src', './img/cat.jpg');
         cardBorder.appendChild(cardImg);
 
-        let cardText = document.createElement('div');
+        const cardText = document.createElement('div');
         cardText.setAttribute('class', 'card-text');
         cardBorder.appendChild(cardText);
 
-        let title = document.createElement('h2');
+        const title = document.createElement('h2');
         title.setAttribute('class', 'title');
         cardText.appendChild(title);
-        title.innerHTML = inpTitle.value;
+        title.textContent = inpTitle.value;
 
-        let content = document.createElement('p');
+        const content = document.createElement('p');
         content.setAttribute('class', 'content');
         cardText.appendChild(content);
-        content.innerHTML = contentText.value;
+        content.textContent = contentText.value;
 
-        let underline = document.createElement('p');
+        const underline = document.createElement('p');
         underline.setAttribute('class', 'underline');
         cardText.appendChild(underline);
-        underline.innerHTML = toDay.getFullYear();
+        underline.textContent = today.toISOString().slice(0, 10);
 
         inpTitle.value = "";
         contentText.value = "";
