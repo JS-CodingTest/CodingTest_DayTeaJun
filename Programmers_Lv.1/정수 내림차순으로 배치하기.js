58a950b051197b0037be368054323b757a37f926
DayTeaJun <gkfnck@gmail.com>
2023-05-11 18:43:53 +0900

    230511 정수 내림차순으로 배치하기

---
+++ Programmers_Lv.1/정수 내림차순으로 배치하기.js
@@ -0,0 +1,17 @@
+// 문제 설명
+// 함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+
+// 제한 조건
+// n은 1이상 8000000000 이하인 자연수입니다.
+function solution(n) {
+  return parseInt(
+    n
+      .toString()
+      .split("")
+      .map((el) => parseInt(el))
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12933
