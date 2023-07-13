d0bac8e61043938b7f509dc55d00db4b6fbaa13d
DayTeaJun <gkfnck@gmail.com>
2023-07-13 23:25:20 +0900

    230713 배열의 유사도

---
+++ Programmers_Lv.0/Lv0 배열의 유사도.js
@@ -0,0 +1,18 @@
+// 문제 설명
+// 두 배열이 얼마나 유사한지 확인해보려고 합니다. 문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ s1, s2의 길이 ≤ 100
+// 1 ≤ s1, s2의 원소의 길이 ≤ 10
+// s1과 s2의 원소는 알파벳 소문자로만 이루어져 있습니다
+// s1과 s2는 각각 중복된 원소를 갖지 않습니다.
+
+function solution(s1, s2) {
+  if (s1.length > s2.length) {
+    return s1.filter((item) => s2.includes(item)).length;
+  } else {
+    return s2.filter((item) => s1.includes(item)).length;
+  }
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120903
