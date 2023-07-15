9a09b1e06ea4501301994e7f5ec354a891d0f24a
DayTeaJun <gkfnck@gmail.com>
2023-07-15 21:10:17 +0900

    230715 정수 부분

---
+++ Programmers_Lv.0/Lv0 정수 부분.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 실수 flo가 매개 변수로 주어질 때, flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 0 ≤ flo ≤ 100
+
+function solution(flo) {
+  return parseInt(flo);
+}
+// trunc라는 소수점이하를 아예 없애는 메서드도 있음
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181850
