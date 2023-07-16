5ab3270bc55521115ae1b3f889f9bb730f464421
DayTeaJun <gkfnck@gmail.com>
2023-07-16 20:02:11 +0900

    230716 문자열 곱하기

---
+++ Programmers_Lv.0/Lv0 문자열 곱하기.js
@@ -0,0 +1,15 @@
+// 문제 설명
+// 문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 100
+// my_string은 영소문자로만 이루어져 있습니다.
+// 1 ≤ k ≤ 100
+
+function solution(my_string, k) {
+  return my_string.repeat(k);
+}
+
+// repeat 메서드는 전체 문자열을 반복해준다
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181940
