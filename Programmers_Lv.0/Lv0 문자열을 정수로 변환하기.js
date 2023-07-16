a63c9389b35b30e6e49d167ab094370698c5f759
DayTeaJun <gkfnck@gmail.com>
2023-07-16 20:05:29 +0900

    230716 문자열을 정수로 변환하기

---
+++ Programmers_Lv.0/Lv0 문자열을 정수로 변환하기.js
@@ -0,0 +1,16 @@
+// 문제 설명
+// 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n_str ≤ 5
+// n_str은 0부터 9까지의 정수 문자로만 이루어져 있습니다.
+
+function solution(n_str) {
+  return parseInt(n_str);
+  // return +n_str;
+  // return Number(n_str);
+  // return n_str/1
+}
+// 문자열에서 정수형으로 변환하는 것에는 계산할 때 자동으로 변환할 수 있다.
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181848
