7c80cd31c0ef95d2d33f1173361c243ddf104818
DayTeaJun <gkfnck@gmail.com>
2023-07-17 22:49:42 +0900

    230717 n번째 원소까지

---
+++ Programmers_Lv.0/Lv0 n번째 원소까지.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 정수 리스트 num_list와 정수 n이 주어질 때, num_list의 첫 번째 원소부터 n 번째 원소까지의 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 2 ≤ num_list의 길이 ≤ 30
+// 1 ≤ num_list의 원소 ≤ 9
+// 1 ≤ n ≤ num_list의 길이
+
+function solution(num_list, n) {
+  return num_list.slice(0, n);
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181889
