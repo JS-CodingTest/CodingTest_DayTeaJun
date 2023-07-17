6739d63bb85f0a75ae53df1f3b8e43f1a3688ff9
DayTeaJun <gkfnck@gmail.com>
2023-07-17 22:55:27 +0900

    230717 문자열로 변환

---
+++ Programmers_Lv.0/Lv0 문자열로 변환.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ n ≤ 10000
+
+function solution(n) {
+  return n.toString();
+  // String(n)
+}
+
+// 문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181845
