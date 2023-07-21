c4662cb7c3e0c2da259d7dd519ace87cc148620e
DayTeaJun <gkfnck@gmail.com>
2023-07-21 18:19:58 +0900

    230721 문자 리스트를 문자열로 변환하기

---
+++ Programmers_Lv.0/Lv0 문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,12 @@
+// 문제 설명
+// 문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+
+// 제한사항
+// 1 ≤ arr의 길이 ≤ 200
+// arr의 원소는 전부 알파벳 소문자로 이루어진 길이가 1인 문자열입니다.
+
+function solution(arr) {
+  return arr.join("");
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/181941
