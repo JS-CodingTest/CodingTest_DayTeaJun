2fc0d3e731163b3fde418ae396d4ffa801203ede
DayTeaJun <gkfnck@gmail.com>
2023-09-06 00:07:27 +0900

    230905 숫자 문자열과 영단어 해설추가

--- Programmers_Lv.1/숫자 문자열과 영단어.js
+++ Programmers_Lv.1/숫자 문자열과 영단어.js
@@ -25,6 +25,7 @@
 // 1 ≤ s의 길이 ≤ 50
 // s가 "zero" 또는 "0"으로 시작하는 경우는 주어지지 않습니다.
 // return 값이 1 이상 2,000,000,000 이하의 정수가 되는 올바른 입력만 s로 주어집니다.
+
 function solution(s) {
   const alpa = [
     "zero",
@@ -40,8 +41,11 @@ function solution(s) {
   ];
   let answer = s;
   for (let i = 0; i < alpa.length; i++) {
-    let arr = answer.split(alpa[i]);
-    answer = arr.join(i);
+    // s가 "one4seveneight"인 것에 i가 만약 1이라면 'one'을 기준으로 자름
+    let arr = answer.split(alpa[i]); // ['', '4seveneight']
+    // 기존 s를 ['', '4seveneight'] 에서 join(1) 을 하여 두개 사이에 0을 합침
+    answer = arr.join(i); // '14seveneight'
+    // 반복
   }
   return Number(answer);
 }
