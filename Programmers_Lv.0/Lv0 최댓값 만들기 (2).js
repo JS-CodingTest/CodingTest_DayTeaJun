a80819edd7c6b498ea6cfd6e561dbef07ece10e5
DayTeaJun <gkfnck@gmail.com>
2023-09-07 23:57:45 +0900

    230907 최댓값 만들기 2

---
+++ Programmers_Lv.0/Lv0 최댓값 만들기 (2).js
@@ -0,0 +1,16 @@
+// 문제 설명
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// -10,000 ≤ numbers의 원소 ≤ 10,000
+// 2 ≤ numbers 의 길이 ≤ 100
+
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+  return Math.max(
+    numbers[0] * numbers[1],
+    numbers[numbers.length - 1] * numbers[numbers.length - 2]
+  );
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/120862
