bee82faf14d0986ad4836d058f0f3b853fbaedf5
DayTeaJun <gkfnck@gmail.com>
2023-09-15 08:17:35 +0900

    230915 올바른 괄호

---
+++ Programmers_Lv.2/올바른 괄호.js
@@ -0,0 +1,21 @@
+// 문제 설명
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+// "()()" 또는 "(())()" 는 올바른 괄호입니다.
+// ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+// '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+// 제한사항
+// 문자열 s의 길이 : 100,000 이하의 자연수
+// 문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+
+function solution(s) {
+  let stackCount = 0;
+  for (let i = 0; i < s.length; i++) {
+    stackCount += s[i] === "(" ? 1 : -1;
+    if (stackCount < 0) return false;
+  }
+  return stackCount === 0 ? true : false;
+}
+
+// !문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
